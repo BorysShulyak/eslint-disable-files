c6b20f3109deb6fdc5f75d4a3cc3e94fe22e1e16
boris.shulyak <borysshulyak@gmail.com>
2023-06-19 13:20:44 +0300

    Introduce `eslint-disable-files` package.

---
+++ src/eslintDisableFiles.js
@@ -0,0 +1,22 @@
+import fs from 'node:fs';
+import path from 'node:path';
+
+import { getDeepFilesFromDir } from '@borisshulyak/node-utils';
+
+const eslintDisableFiles = (rootDir = './', pattern = /\.[cm]?[jt]sx?$/) => {
+  const files = getDeepFilesFromDir(rootDir, pattern);
+
+  files.forEach((file) => {
+    const filePath = path.join(rootDir, file);
+
+    const contents = fs.readFileSync(filePath, 'utf8');
+    if (!contents.trim().startsWith('/* eslint-disable */')) {
+      const updatedContents = `/* eslint-disable */\n\n${contents}`;
+      fs.writeFileSync(filePath, updatedContents);
+    }
+  });
+};
+
+eslintDisableFiles();
+
+export default eslintDisableFiles;
---
+++ src/index.js
@@ -0,0 +1,2 @@
+// eslint-disable-next-line import/prefer-default-export
+export { default as eslintDisableFiles } from './eslintDisableFiles.js';
